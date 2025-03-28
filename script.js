1701dcdd913c51d896111418f8943323ea474305
Владимир Яцулевич <vovas53@yandex.ru>
2025-03-28 21:04:23 +0300

    Button+input

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("Hello world!");
+// alert("Hello world!");
+console.log("Hello world!");
+
+var x = 2;
+var y = 3;
+console.log(x + y);
+
+function my_click() {
+    // console.log("Ты тыкнул на кнопку");
+    var s = document.getElementById("input_field").value;
+    console.log(s);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Весь код будет работать после загрузки страницы
+    // var s = document.getElementById("text").innerHTML;
+    // console.log(s);
+
+    document.getElementById("text").innerHTML = "I am hacker";
+})
+
